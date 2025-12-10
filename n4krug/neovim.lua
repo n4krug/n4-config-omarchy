91d36d7c8c649b0b93d274769ab2bc0a734e64fc
Gustav Eneberg <gustav.eneberg@gmail.com>
2025-12-10 01:41:25 +0100

    added theme back in

---
+++ n4krug/neovim.lua
@@ -0,0 +1,14 @@
+return function(use)	
+  use {
+		"steve-lohmeyer/mars.nvim",
+		name = "mars",
+	}
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = {
+	-- 		colorscheme = "mars",
+	-- 	},
+	-- },
+
+  require('mars').setup()
+end
